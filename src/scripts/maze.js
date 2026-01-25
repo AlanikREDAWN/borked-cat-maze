ffdc9e03af1de3bb606afd2ff557f576716ad203
AlanikREDAWN <lilia.dostie@socprep.org>
2026-01-25 15:41:07 -0500

    functioning, starting to add style

--- src/scripts/maze.js
+++ src/scripts/maze.js
@@ -64,8 +64,21 @@ class Maze {
         }
 
         if (this.stack.length == 0) {
+            
             current.undoHighlight(this.columns);
             canMove = true;
+            
+            const exitCell = this.grid[this.rows - 1][this.columns - 1];
+            exitCell.walls.rightWall = false;
+            exitCell.show(this.size, this.rows, this.columns);
+            ctx.fillStyle = "green";
+            ctx.fillRect(
+                exitCell.colNum * this.size / this.columns + 1,
+                exitCell.rowNum * this.size / this.rows + 1,
+                this.size / this.columns - 2,
+                this.size / this.rows - 2
+            );
+
             spawnPlayer(playerX, playerY);
             return;
         }
@@ -161,6 +174,7 @@ class Cell {
     }
 
     removeWalls(cell1, cell2) {
+        
         let x = (cell1.colNum - cell2.colNum);
 
         if (x == 1) {
@@ -183,6 +197,7 @@ class Cell {
     }
 
     show(size, rows, columns) {
+
         let x = (this.colNum * size) / columns;
         let y = (this.rowNum * size) / rows;
 
@@ -206,37 +221,134 @@ newMaze.draw()
 
 document.addEventListener("keydown", handleKey);
 
+const directions = ['up', 'down', 'left', 'right'];
+
+// const keyboardValues = ['ArrowUp', 'ArrowDown', 'ArrowLeft', 'ArrowRight', 'w', 'a', 's', 'd'];
+
+
+const keyboardValues = ['w', 'a', 's', 'd'];
+const randomKeyMappings = {};
+
+directions.forEach(direction => {
+    const randomKey = keyboardValues[Math.floor(Math.random() * keyboardValues.length)];
+    
+    const index = keyboardValues.indexOf(randomKey);
+    if (index > -1) { 
+    keyboardValues.splice(index, 1);
+}
+    const propertyName = direction
+    randomKeyMappings[propertyName] = {
+        key: randomKey,
+        direction: direction
+    };
+});
+
+console.log("move up:", randomKeyMappings['up'].key)
+console.log("move down:", randomKeyMappings['down'].key)
+console.log("move left:", randomKeyMappings['left'].key)
+console.log("move right:", randomKeyMappings['right'].key)
+// for (let i = 0; i < 4; i++) {
+//     const randomKey = keyboardValues[Math.floor(Math.random() * keyboardValues.length)];
+//     // const randomDirection = directions[Math.floor(Math.random() * directions.length)];
+//     const propertyName = `input_${i}`;
+
+//     dynamicKeyMappings[propertyName] = {
+//         key: randomKey,
+//         direction: randomDirection
+//     };
+// }
+
+// function pickKeys() {
+//     var keyOptions = ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "a", "s", "d", "f", "g", "h", "j", "k", "l", "z", "x", "c", "v", "b", "n", "m"];
+//     var keyOptionsNum = keyOptions.length;
+//     var directions = ['up', 'down', 'left', 'right'];
+//     var directionsNum = directions.length;
+
+    
+//     for (var direction = 0; direction < directionsNum; direction++) {
+//         randomKeyInt = Math.floor(Math.random() * keyOptions.length);
+//         let randomKey = keyOptions[randomKeyInt];
+        
+
+        
+//     }
+// }
+
 function handleKey(e) {
     if (canMove == true) {
-        if (e.key == "w") {
+        if (e.key == randomKeyMappings['up'].key) {
             movePlayer("up");
         }
-        if (e.key == "s") {
+        if (e.key == randomKeyMappings['down'].key) {
             movePlayer("down");
         }
-        if (e.key == "a") {
+        if (e.key == randomKeyMappings['left'].key) {
             movePlayer("left");
         }
-        if (e.key == "d") {
+        if (e.key == randomKeyMappings['right'].key) {
             movePlayer("right");
         }
     }
+    // if (canMove == true) {
+    //     if (e.key == "w") {
+    //         movePlayer("up");
+    //     }
+    //     if (e.key == "s") {
+    //         movePlayer("down");
+    //     }
+    //     if (e.key == "a") {
+    //         movePlayer("left");
+    //     }
+    //     if (e.key == "d") {
+    //         movePlayer("right");
+    //     }
+    // }
 
 }
 
 const TILE = 32;
 
 function movePlayer(direction) {
-    let checkX = playerX;
-    let checkY = playerY;
+    let nextX = playerX;
+    let nextY = playerY;
 
     switch (direction) {
-        
+        case 'up': nextY -= TILE; break;
+        case 'down': nextY += TILE; break;
+        case 'left': nextX -= TILE; break;
+        case 'right': nextX += TILE; break;
     }
 
+    if (!isTileBlocked(nextX, nextY, direction)) {
+        playerX = nextX;
+        playerY = nextY;
+        player.style.left = `${playerX}px`;
+        player.style.top = `${playerY}px`;
+    }
+    // console.log("trying to move", direction);
     // let nextX = playerX;
     // let nextY = playerY;
-    let validMove = false;
+
+    // switch (direction) {
+    //     case 'up': nextY -= TILE; break;
+    //     case 'down': nextY += TILE; break;
+    //     case 'left': nextX -= TILE; break;
+    //     case 'right': nextX += TILE; break;
+    // }
+
+    // // if (true) {
+    // if (!isTileBlocked(nextX, nextY)) {
+    //     playerX = nextX;
+    //     playerY = nextY;
+
+    //     player.style.left = `${playerX}px`;
+    //     player.style.top = `${playerY}px`;
+    //     // player.style.transform = `translate(${playerX}px, ${playerY}px)`;
+    // }
+
+    // // let nextX = playerX;
+    // // let nextY = playerY;
+    // let validMove = false;
 
     // switch (direction) {
     //     case 'up': nextY--; break;
@@ -296,7 +408,7 @@ function spawnPlayer(x, y) {
 
     player = document.createElement('img');
 
-    player.src = 'src/images/burger.png';
+    player.src = 'src/assets/images/burger.png';
     player.alt = "burger player";
     player.classList.add('player');
 
@@ -328,4 +440,75 @@ function isCollision(x, y) {
     //     return true;
     // } 
     // return false;
+}
+
+function isTileBlocked(tileX, tileY, direction) {
+    const col = tileX / TILE;
+    const row = tileY / TILE;
+
+    if (row < 0 || row >= newMaze.rows || col < 0 || col >= newMaze.columns) return true;
+
+    const cell = newMaze.grid[row][col];
+
+    switch (direction) {
+        case "up":
+            return cell.walls.bottomWall;
+        case "down":
+            return cell.walls.topWall;
+        case "left":
+            return cell.walls.rightWall;
+        case "right":
+            return cell.walls.leftWall;
+    }
+    return true;
+    // const rect = maze.getBoundingClientRect();
+
+    // const startX = Math.floor(tileX - rect.left + 4);
+    // const startY = Math.floor(tileY - rect.top + 4);
+    // const width = TILE - 8;
+    // const height = TILE - 8;
+    // // const endX = startX + TILE - 8;
+    // // const endY = startY + TILE - 8;
+
+    // if (
+    //     startX < 0 || startY < 0 ||
+    //     startX + width >= maze.width || 
+    //     startY + height >= maze.height
+    // ) {
+    //     return true;
+    // }
+
+    // const data = ctx.getImageData(startX, startY, width, height).data;
+
+    // for (let i = 0; i < data.length; i += 4) {
+    //     const r = data[i];
+    //     const g = data[i + 1];
+    //     const b = data[i + 2];
+
+    //     if (r < 20 && g < 20 && b < 20) {
+    //         return false;
+    //     }
+    // }
+    // return true;
+
+    // // if (cx < 0 || cy < 0 || cx >= maze.width || cy >= maze.height) {
+    // //     return true;
+    // // }
+    // // // ctx.fillStyle = "red";
+    // // // ctx.fillRect(cx, cy, 2, 2);
+    // // const imageData = ctx.getImageData(cx, cy, 1, 1);
+    // // const [r, g, b] = imageData.data;
+
+    // // const isPath = r > 240 && g > 240 && b > 240;
+    // // return !isPath;
+}
+
+let wait = document.querySelector('.wait');
+
+setInterval(checkForCompletion, 1000);
+
+function checkForCompletion() {
+    if (canMove === true) {
+        wait.remove();
+    }
 }
